25e091ae1031783cc79ad02f0ef5ac7ee7389732
Jake Petty <support@vexcon.net>
2019-04-14 14:42:06 -0700

    Added ISS information bar

--- js/website.js
+++ js/website.js
@@ -3,8 +3,10 @@ class Website {
     constructor() {
         let self = this;
         this._map = null;
-        this._marker = null;
-        $.getScript("https://maps.google.com/maps/api/js?sensor=false&key=AIzaSyByLobYLYqhklGiVYWVuRPbdzhYYkPYO9w", function (a) {
+        this._issMarker = null;
+        this._homeMarker = null;
+        this._geocoder = null;
+        $.getScript("https://maps.google.com/maps/api/js?sensor=false&key=AIzaSyByLobYLYqhklGiVYWVuRPbdzhYYkPYO9w&libraries=geometry", function (a) {
             self.setupMap();
         });
     }
@@ -29,7 +31,7 @@ class Website {
                 }, {
                     "featureType": "administrative.province",
                     "elementType": "geometry.stroke",
-                    "stylers": [{ "visibility": "on" }, { "color": "#363b4a" }, { "lightness": "-30" }]
+                    "stylers": [{ "visibility": "on" }, { "color": "#8c99bf" }, { "lightness": "-30" }]
                 }, {
                     "featureType": "administrative.locality",
                     "elementType": "labels",
@@ -53,18 +55,56 @@ class Website {
                 }
             ]
         });
-        this._marker = new google.maps.Marker({
+
+        this._geocoder = new google.maps.Geocoder()
+        this._issMarker = new google.maps.Marker({
             map: this._map,
             optimized: false,
             icon: new google.maps.MarkerImage('img/iss_marker.png', new google.maps.Size(250, 250), new google.maps.Point(0, 0), new google.maps.Point(250 / 2, 250 / 2))
         });
+
+        this._homeMarker = new google.maps.Marker({
+            map: this._map,
+            optimized: false,
+            icon: new google.maps.MarkerImage('img/home_marker.png', new google.maps.Size(52, 52), new google.maps.Point(0, 0), new google.maps.Point(52 / 2, 52 / 2)),
+            position: {
+                lat: 41.978119,
+                lng: -91.667814
+            }
+        });
         this.setupISS(this);
     }
     setupISS(self) {
-        $.getJSON('https://api.wheretheiss.at/v1/satellites/25544', function (payload) {
+        $.getJSON('https://api.wheretheiss.at/v1/satellites/25544?units=miles', function (payload) {
             let latlng = { lat: payload.latitude, lng: payload.longitude };
-            self._marker.setPosition(latlng);
-            self._map.setCenter(latlng);
+            self._issMarker.setPosition(latlng);
+
+            let bounds = new google.maps.LatLngBounds();
+            bounds.extend(self._issMarker.getPosition());
+            bounds.extend(self._homeMarker.getPosition());
+            self._map.fitBounds(bounds);
+
+            self._geocoder.geocode({ location: latlng }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    let addrComponents = results[0].address_components;
+                    let location;
+                    for (let i = 0; i < addrComponents.length; i++) {
+                        if (addrComponents[i].types[1] == "political") {
+                            location = addrComponents[i].long_name + ', ';
+                        }
+                        if (addrComponents[i].types[0] == "country") {
+                            location += addrComponents[i].long_name;
+                        }
+                    }
+                    $('#iss-location').text(location);
+                } else {
+                    $('#iss-location').text("Over an Ocean");
+                }
+            });
+            var distance = google.maps.geometry.spherical.computeDistanceBetween(self._homeMarker.getPosition(), self._issMarker.getPosition()) * 0.000621371;
+            $("#iss-distance").text(Number(distance.toFixed()).toLocaleString("en-US")).append("<span class='units'>miles</span>")
+            $('#iss-velocity').text(Number(payload.velocity.toFixed()).toLocaleString("en-US")).append("<span class='units'>mph</span>")
+            $('#iss-altitude').text(Number(payload.altitude.toFixed()).toLocaleString("en-US")).append("<span class='units'>miles</span>")
             setTimeout(function () {
                 self.setupISS(self)
             }, 5000);
